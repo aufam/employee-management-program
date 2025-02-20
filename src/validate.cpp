f5b07d38a3f3c0653a8665d00b07c88e98d0e581
aufam <aufamuhammad715@gmail.com>
2025-02-20 10:06:22 +0700

    check not empty

--- src/validate.cpp
+++ src/validate.cpp
@@ -9,7 +9,11 @@ using delameta::Result, delameta::Error;
 using delameta::http::Status;
 using etl::Ok, etl::Err;
 
+#define CHECK_NOT_EMPTY(field) if (field.empty()) return Err(Error{Status::StatusBadRequest, "Field cannot be empty"})
+
 auto finshot::Employee::ValidateField(const std::string& field) -> Result<void> {
+    CHECK_NOT_EMPTY(field);
+
     for (char ch: field) {
         if (ch == ',') return Err(Error{Status::StatusBadRequest, "Field value cannot contains `,`"});
         if (ch == ';') return Err(Error{Status::StatusBadRequest, "Field value cannot contains `;`"});
@@ -19,6 +23,8 @@ auto finshot::Employee::ValidateField(const std::string& field) -> Result<void> 
 }
 
 auto finshot::Employee::ValidateId(const std::string& id) -> Result<void> {
+    CHECK_NOT_EMPTY(id);
+
     if (id.size() == 3 && std::all_of(id.begin(), id.end(), ::isdigit))
         return Ok();
 
@@ -26,6 +32,8 @@ auto finshot::Employee::ValidateId(const std::string& id) -> Result<void> {
 }
 
 auto finshot::Employee::ValidatePhoneNumber(const std::string& phone) -> Result<void> {
+    CHECK_NOT_EMPTY(phone);
+
     std::regex phoneRegex(R"(^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$)");
     if (std::regex_match(phone, phoneRegex))
         return Ok();
@@ -34,6 +42,8 @@ auto finshot::Employee::ValidatePhoneNumber(const std::string& phone) -> Result<
 }
 
 auto finshot::Employee::ValidateEmailAddress(const std::string& email) -> Result<void> {
+    CHECK_NOT_EMPTY(email);
+
     std::regex emailRegex(R"(^[_a-z0-9-]+(\.[_a-z0-9-]+)*@(?:\w+\.)+\w+$)");
     if (std::regex_match(email, emailRegex))
         return Ok();
